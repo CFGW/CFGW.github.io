5fad04455c73e9b9c3748752e3163cd6b7a190c6
Fameron <cfoster92@live.com.au>
2017-06-19 13:01:18 +1000

    test sub page
    trying to add strait crewing demo

---
+++ sc/html/assets/touch01/touch01_edgeActions.js
@@ -0,0 +1,14 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){this.play(0);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+})(jQuery,AdobeEdge,"EDGE-250788476");
