c428ed258552825acd022ef14066de74723b78f2
Fameron <cfoster92@live.com.au>
2017-07-25 00:41:29 +1000

    folio redesign
    Overhaul of my previous folio page

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
+// display first textbody block
+$("#one").css({ 'display': "block" });
+
+// show/hide relevant textbody on link click
 $(document).ready(function () {
   $('a.links').click(function (e) {
     e.preventDefault();
-    var three = $('a.links').index($(this))
-    $('.divs').hide().eq(three).show();
+    var showDiv = $('a.links').index($(this))
+    $('.divs').hide().eq(showDiv).show();
   });
+});
+
+//show email and edit href for email on div hover
+$(".contact-button").hover(function() {
+  $(".addContact").html('cfgw@protonmail.com').prop("href","mailto:cfgw@protonmail.com");
+}, function() {
+  $(".addContact").html('Lets work together').prop("href","#");
 });
